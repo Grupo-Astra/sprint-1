6fad27e26523f38523c61795e1eae0b96809a638
jsvictorias <juliasantos7652@gmail.com>
2025-05-17 21:31:03 -0300

    add: inicializando o projeto, configuração da primeira tela e instalei as fontes

---
+++ pages/splash-screen/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+export const SplashScreen = () => {
+  return (
+    <S.SplashContainer>
+      <h1>FESTO</h1>
+      <h1>OSJJSD</h1>
+    </S.SplashContainer>
+  );
+};
---
+++ pages/splash-screen/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const SplashContainer = styled.View`
+  background-color: #0294a6;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
