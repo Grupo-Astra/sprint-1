6b4e7b8735834369c1a7046eee31261a88de6dcf
jsvictorias <juliasantos7652@gmail.com>
2025-05-17 22:01:30 -0300

    feat: configuração da duração do splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
+import { Home } from "@/pages/home/Home";
 import { SplashScreen } from "@/pages/splash-screen/SplashScreen";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
-  return <SplashScreen />;
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <>{showSplash ? <SplashScreen /> : <Home />}</>;
 }
