0b088aa6e8a8fc3cc4b52bdf711b8e08ed774ce3
jsvictorias <juliasantos7652@gmail.com>
2025-05-18 19:01:45 -0300

    add: página home completa

--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -1,14 +1,37 @@
 import { NavBar } from "@/components/navbar/Navbar";
+import { ScrollView } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import * as S from "./styles";
 
 export const Home = () => {
   return (
-    <S.HomeContainer>
-      <S.HomeTitle>Bem-vindo</S.HomeTitle>
-        
-      <S.Nav>
-        <NavBar />
-      </S.Nav>
-    </S.HomeContainer>
+    <SafeAreaProvider>
+      <S.HomeContainer>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <S.ContentWrapper>
+            <S.HomeTitle>ASTRA</S.HomeTitle>
+            <S.Presentation>
+              O projeto desenvolvido para a Festo consiste na criação de um
+              gêmeo digital de um sistema pneumático de balancim utilizado no
+              corte de materiais para calçados. Que tem por objetivos simular
+              digitalmente o equipamento físico em operação, monitorar em tempo
+              real o status de todos os componentes, prevenir falhas através de
+              análises preditivas e reduzir tempo de inatividade por manutenção.
+            </S.Presentation>
+          </S.ContentWrapper>
+
+          <S.ImageWrapper>
+            <S.LogoImage
+              source={require("../../assets/images/logoBranca.png")}
+              style={{ paddingBottom: 120 }}
+            />
+          </S.ImageWrapper>
+        </ScrollView>
+
+        <S.Nav>
+          <NavBar />
+        </S.Nav>
+      </S.HomeContainer>
+    </SafeAreaProvider>
   );
 };
--- pages/home/styles.ts
+++ pages/home/styles.ts
@@ -1,23 +1,44 @@
 import { Fonts } from "@/styles/Fonts";
 import styled from "styled-components/native";
 
-export const HomeContainer = styled.View`
+export const HomeContainer = styled.SafeAreaView`
   flex: 1;
   background-color: #f0f0f0;
-  width: 100%;
   justify-content: space-between;
 `;
 
+export const ContentWrapper = styled.View`
+  flex: 1;
+  justify-content: flex-start;
+`;
+
 export const HomeTitle = styled.Text`
-  font-size: 4rem;
+  font-size: 70px;
   font-family: ${Fonts.Cinzel};
   text-align: center;
-  margin-top: 5rem;
+  margin-top: 40px;
+  margin-bottom: 30px;
+  font-weight: 700;
 `;
-export const FlatCont = styled.View`
-  width: 100%;
-  padding: 1.5rem;
+
+export const Presentation = styled.Text`
+  font-family: ${Fonts.Cinzel};
+  font-size: 25px;
+  text-align: justify;
+  padding: 0 20px;
 `;
+
+export const ImageWrapper = styled.View`
+  align-items: center;
+  /* margin-bottom: 30px; */
+`;
+
+export const LogoImage = styled.Image`
+  width: 150px;
+  height: 150px;
+  resize: contain;
+`;
+
 export const Nav = styled.View`
   margin-bottom: 0;
 `;
